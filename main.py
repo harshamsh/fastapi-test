df7a130fbe76352db99639613e54f51b5555739b
Sri Harsha Musunuru <57175686+harshamsh@users.noreply.github.com>
2023-05-22 16:48:31 +0100

    port correction

--- main.py
+++ main.py
@@ -107,4 +107,4 @@ async def websocket_endpoint(websocket: WebSocket):
     )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0")
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
